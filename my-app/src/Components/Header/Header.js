6abe2e71ffca51e297c9ee5c6217ff4fe6a89b1b
OdinsBeard82 <danielcharles_500@hotmail.com>
2024-06-17 13:45:40 +0100

    Just Home page

--- my-app/src/Components/Header/Header.js
+++ my-app/src/Components/Header/Header.js
@@ -4,17 +4,12 @@ import './Header.css';
 function Header() {
 
   
-
   return (
     <div className="header">
       <h1 className="header-title">NBA Shop</h1>
       <nav>
         <ul className="nav-list">
           <li><a href="/">Home</a></li>
-          <li><a href="/shop">Shop</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
-          <li><a href="/cart">Cart</a></li> 
         </ul>
       </nav>
     </div>
